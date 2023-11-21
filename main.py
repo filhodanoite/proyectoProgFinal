7ba20915cfb0b96998b480aec6e256af08fa1475
filhodanoite <jeremias.ramirez@sagradoalcal.edu.ar>
2023-11-21 15:16:04 -0300

    Version Final

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import random
+from tkinter import messagebox
 
 ventana = tk.Tk()
 ventana.title("Quiz de Fútbol")
@@ -9,12 +10,14 @@ clubes = []
 opciones = []
 club_elegido = None
 puntaje_total = 0
+puntaje_acumulado = 0
 vidas = 3
+vidas_generales = 3
 
 ruta_imagenes = "/home/alcal/Descargas/jortge/Escudos/"
 
 ligas_y_clubes = {
-    "Premier League": ["Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton And Hove Albion", "Burnley", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Liverpool", "Luton", "Manchester City", "Manchester United", "Newcastle United", "Nottingham Forest", "Sheffield United", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers"],
+    "Premier League": ["Arsenal", "Aston Villa", "Bournemouth", "Brentford", "Brighton And Hove Albion", "Burnley", "Chelsea", "Crystal Palace", "Everton", "Fulham", "Liverpool", "Luton", "Manchester City", "Manchester United", "Newcastle United", "Nottingham Forest", "Sheffield United", "Tottenham Hotspur", "West Ham United", "Wolverhampton Wanderers",],
     "Bundes Liga": ["FC Augsburg", "Bayer Leverkusen", "FC Bayern München", "VFL Bochum 1848", "Borussia Dortmund", "Borussia Mönchengladbach", "FC Köln", "SV Darmstadt 98", "Eintracht", "Sport-Club Freiburg", "1. FC Heidenheim 1846", "TSG 1899 Hoffenheim","RB Leipzig", "1. FSV Mainz 05", "VFB Stuttgart", "FC Union Berlin", "SV Werder Bremen", "VFL Wolfsburg"],
     "La liga" : ["Athletic Club", "Atletico De Madrid", "CA Osazuna", "Cadiz CF", "Deportivo Alaves", "FC Barcelona", "Getafe CF", "Girona FC", "Granada CF", "Rayo Vallecano", "RC Celta", "Real Betis", "Real Madrid", "Real Sociedad", "Sevilla FC", "UD Almeria", "Valencia CF", "Villareal CF"],
     "Serie A" : ["Atalanta", "Bologna", "Cagliari", "Empoli F.C", "Fiorentina", "Frosinone", "Genoa", "Internazionale", "Juventus", "Lazio", "Lecce", "Milan","Monza","Napoli","Roma","U.S Salernitana 1919","Sassuolo","Torino","Udinese","Verona"],
@@ -24,11 +27,10 @@ ligas_y_clubes = {
 
 
 def iniciar_juego(liga_seleccionada):
-    global clubes, opciones, club_elegido, puntaje_total, vidas
-    mensaje_label.config(text="")
+    global clubes, opciones, club_elegido, puntaje_total, vidas, vidas_generales, puntaje_acumulado
+    resultado.config(text="")
     clubes = ligas_y_clubes[liga_seleccionada].copy()
-    vidas = 3
-    puntaje_total = 0
+    vidas = vidas_generales  # Usar vidas generales en lugar de reiniciarlas en cada juego
     seleccionar_club_random()
 
 def seleccionar_club_random():
@@ -63,24 +65,22 @@ def seleccionar_club_random():
         print(f"Imagen no encontrada para {club_elegido} en la ruta {escudo_path}")
 
 def verificar_respuesta(respuesta):
-    global puntaje_total, vidas, opciones, club_elegido
+    global puntaje_total, vidas, vidas_generales, puntaje_acumulado, opciones, club_elegido
     if vidas > 0:
         if respuesta == club_elegido:
             puntaje_total += 1
             resultado.config(text="¡Correcto! Puntaje: {}".format(puntaje_total), bg="green")
         else:
             vidas -= 1
-            if vidas == 0:
+            vidas_generales -= 1  # Reducir las vidas generales
+            if vidas_generales == 0:
                 mostrar_puntaje_final()
                 for boton in botones_respuesta:
                     boton.config(state=tk.DISABLED)
-                opciones = []  # Borramos las opciones al quedarse sin vidas
+                opciones = []  # Borramos las opciones al quedarse sin vidas generales
             else:
                 resultado.config(text="Incorrecto. La respuesta correcta es: {}".format(club_elegido), bg="red")
-                if vidas >= 2:
-                    mensaje_label.config(text="Tienes {} vidas restantes.".format(vidas))
-                elif vidas == 1:
-                    mensaje_label.config(text="Tienes {} vida restante.".format(vidas))
+                mensaje_label.config(text="Tienes {} vidas restantes.".format(vidas_generales))
 
         for boton in botones_respuesta:
             boton.config(state=tk.DISABLED)
@@ -97,13 +97,62 @@ def ocultar_resultado():
         mostrar_puntaje_final()
 
 def mostrar_puntaje_final():
-    mensaje_label.config(text="Juego terminado. Puntaje total: {}".format(puntaje_total))
-    
+    mensaje = "Juego terminado. Puntaje total: {}".format(puntaje_total)
+    messagebox.showinfo("Fin del Juego", mensaje)
+    reiniciar_juego()
+
+def reiniciar_juego():
+    global puntaje_total, vidas_generales, puntaje_acumulado
+    vidas_generales = 3  # Reiniciar las vidas generales
+    puntaje_acumulado += puntaje_total
+    puntaje_total = 0
+    liga_var.set(liga_var.get())  # Establecer liga_var con el valor seleccionado
+    mensaje_label.config(text="")
+    for boton in botones_respuesta:
+        boton.config(state=tk.NORMAL)
+
+    reiniciar = messagebox.askquestion("Fin del Juego", "¿Quieres jugar de nuevo?", icon='question')
+
+    if reiniciar == 'yes':
+        iniciar_juego(liga_var.get())
+    else:
+        ventana.destroy()
+
 
 def mostrar_mensaje_final():
+    global puntaje_acumulado
     mensaje_label.config(text="Has visto todos los escudos de esta liga. Puntaje total: {}".format(puntaje_total))
     for boton in botones_respuesta:
         boton.config(state=tk.DISABLED)
+
+def mostrar_tutorial():
+    tutorial_texto = """
+    Bienvenido al Quiz de Fútbol:
+
+    - Selecciona una liga en el menú desplegable.
+    
+    - Haz clic en 'Comenzar Juego' para iniciar.
+
+    - Se mostrará el escudo de un club, y debes elegir el nombre correcto entre las opciones.
+
+    - Tienes vidas, y perderás una si te equivocas.
+
+    - Puedes ver tu puntaje total y las vidas restantes.
+
+    - Si decides salir o perder todas las vidas, puedes reiniciar el juego.
+
+    ¡Diviértete y buena suerte!
+    """
+
+    tutorial_font = ("Arial", 14)  # Cambia el tamaño de la fuente según tus preferencias
+
+    # Crear una ventana emergente personalizada para el tutorial
+    ventana_tutorial = tk.Toplevel(ventana)
+    ventana_tutorial.title("Tutorial")
+
+    # Crear una etiqueta con el texto del tutorial y aplicar la fuente
+    label_tutorial = tk.Label(ventana_tutorial, text=tutorial_texto, font=tutorial_font, padx=20, pady=20)
+    label_tutorial.pack()
 
 etiqueta_liga = tk.Label(ventana, text="Selecciona una liga:")
 etiqueta_liga.pack()
@@ -127,6 +176,9 @@ for i in range(4):
 
 resultado = tk.Label(ventana, text="")
 resultado.pack()
+
+boton_tutorial = tk.Button(ventana, text="Tutorial", command=mostrar_tutorial)
+boton_tutorial.pack(side=tk.BOTTOM)
 
 mensaje_label = tk.Label(ventana, text="")
 mensaje_label.pack()
